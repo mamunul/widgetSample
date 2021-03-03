9f6806fe58cde25947259660226890b18769f697
Mamunul Mazid <mamunul.mazid@gmail.com>
2021-03-03 14:56:50 +0600

    Initial Commit

---
+++ WidgetExample/WidgetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetExampleApp.swift
+//  WidgetExample
+//
+//  Created by New User on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct WidgetExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
